ff2a56cbfa958b4e9611e8fc6b7e940d95a47075
Gj <gongjie0422@163.com>
2021-03-19 10:57:40 +0800

    fix: modify

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,10 @@
 
 /**
  * a标签文件下载
- * @param target 字符串
- * @param fileName 文件名
+ * @param {string} target 字符串
+ * @param {string} fileName 文件名
  */
-export function SimpleDownload(
+export function simpleDownload(
     target: string,
     fileName: string
 ): void
