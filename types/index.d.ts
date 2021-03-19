8e226b4ff76fa55b7f269345a731bfb9eb4b53c3
Gj <gongjie0422@163.com>
2021-03-19 11:18:03 +0800

    fix: modify

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,9 @@
  * a标签文件下载
  * @param {string} target 字符串
  * @param {string} fileName 文件名
+ * ```
+ * simpleDownload(JSON.stringify({ alrale: 'simpleDownload' }, null, '\t'), 'alrale.json')
+ * ```
  */
 export function simpleDownload(
     target: string,
